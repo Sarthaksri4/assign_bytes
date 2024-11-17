0d4e23fd84250f3a3479f7a18de71abff591200d
sarthak <sarthaksri044@gmail.com>
2024-11-17 19:08:52 +0530

    Nav

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+'use client';
+import Navbar from '@/Components/Nav'
+import React from 'react'
+
+const page = () => {
+  
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default page
